32ccfda73015647d6532303407757b0affed1a71
Vadim Yuzmekhametov <vadimshift@yandex.ru>
2021-02-25 13:51:24 +0500

    понаделал красоту в формВалидаторе

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -10,42 +10,46 @@ export class FormValidator {
     this._inputTypeError = config.inputTypeError
 
   }
+  //включаем валидацию
   enableValidation() {
     this._setEventListeners()
-
   }
 
+  //добавляем обработчики всем инпутам
   _setEventListeners() {
-
-    this._allInputs = Array.from(this._form.querySelectorAll(this._inputSelector))
+    this._allInputs = Array.from(this._form.querySelectorAll(this._inputSelector)) //делаем массив из всех инпутов
     this._submitButton = this._form.querySelector(this._submitButton)
-    this._submitButtonStatus(this._allInputs)
+    this._submitButtonStatus(this._allInputs) //запускаем проверку статуса кнопки, в момент открытия формы
     this._allInputs.forEach((inputElement) => {
       // каждому полю добавляем обработчик события input
       inputElement.addEventListener('input', () => {
-        // Вызываем isValid, передаем форму и инпут
+        // Вызываем isValidInput, передаем форму и инпут
         this._isValidInput(inputElement)
-        this._submitButtonStatus(this._allInputs, this._submitButton);
+        this._submitButtonStatus(this._allInputs, this._submitButton)
       })
     })
-
   }
+
+  //проверяем валидность инпута
   _isValidInput(inputElement) {
     if (!inputElement.validity.valid) {
       // Если поле не проходит валидацию, покажем ошибку
-      this._showInputError(inputElement, inputElement.validationMessage);
+      this._showInputError(inputElement, inputElement.validationMessage)
     } else {
-      // Если проходит, скроем
-      this._hideInputError(inputElement);
+      // Если проходит, скроем ошибку
+      this._hideInputError(inputElement)
     }
   }
+
+  //показываем сообщения об ошибке в инпутах
   _showInputError(inputElement, errorMessage) {
     this._errorElement = this._form.querySelector(`.${inputElement.id}-error`)
     inputElement.classList.add(this._inputTypeError)
-    this._errorElement.textContent = errorMessage;
+    this._errorElement.textContent = errorMessage
     this._errorElement.classList.add(this._errorText)
   }
 
+  //скрываем сообщения об ошибке в инпутах
   _hideInputError(inputElement) {
     this._errorElement = this._form.querySelector(`.${inputElement.id}-error`)
     inputElement.classList.remove(this._inputTypeError)
@@ -53,26 +57,27 @@ export class FormValidator {
     this._errorElement.textContent = ''
   }
 
+  //проверяем наличие невалидного инпута
   _inputCheckValidity(allInputs) {
     return allInputs.some((inputElement) => {
       // Если поле не валидно, вернем true
       // Обход массива прекратится и вся фунцкция
       // inputCheckValidity вернёт true
-      return !inputElement.validity.valid;
+      return !inputElement.validity.valid
     })
   }
+
+  //меняем статус кнопки в зависимости от валидности инпутов
   _submitButtonStatus(allInputs) {
     if (this._inputCheckValidity(allInputs)) {
       // делаем кнопку неактивной
-      this._submitButton.classList.add(this._submitButtonDisabled);
+      this._submitButton.classList.add(this._submitButtonDisabled)
       this._submitButton.setAttribute('disabled', true)
     } else {
       // иначе делаем кнопку активной
-      this._submitButton.classList.remove(this._submitButtonDisabled);
+      this._submitButton.classList.remove(this._submitButtonDisabled)
       this._submitButton.removeAttribute('disabled')
     }
   }
-
-
 }
 

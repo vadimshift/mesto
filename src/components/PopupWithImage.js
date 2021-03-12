f8e055bbeb57b4052499aa17a8afbd868bcf445d
vadimshift <vadimshift@yandex.ru>
2021-03-12 20:26:56 +0500

    исправил еще 1 ошибку

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,11 +2,11 @@ import { Popup } from '../components/Popup.js'
 export class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    const img = this._popup.querySelector('.popup__image-xl')
+    const title = this._popup.querySelector('.popup__title_type_image-xl')
   }
 
   open(name, link) {
-    const img = this._popup.querySelector('.popup__image-xl')
-    const title = this._popup.querySelector('.popup__title_type_image-xl')
     img.src = link
     title.textContent = name
     super.open()
